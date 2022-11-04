b9f884c212b27c4ccefc4f7b9226f848d4221859
ralph <ralph@localhost>
2022-11-04 12:23:06 +0100

    add middleware and routes

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const server = http.createServer(app);
 const config = require('./data/config.json');
-const { MODUS_SESSION_TYPE, ModusSession } = require('./lib/session/session');
-const { sendResponseObject, constructResponseObject } = require('./lib/util/api');
+const { loadAllRoutes } = require('./lib/router/routeloader');
 
 // Import cookie parser
 app.use(cookieParser("secret"));
@@ -26,56 +25,15 @@ app.use(bodyParser.json());
  * Contains the current application state.
  */
 const appContext = {
+    app,
     wsEventQueue: require('./lib/event/wsequeue'),
     sessionMgr: require('./lib/session/sessmgr')
 }
 
-// [ Session Routes ]
-// Provides session management routes to be used by the client.
-// Initializes a session
-// Also does auth checks to ensure a non-user session (kitchen view or admin session) can't be created without the right authorization.
-app.post('/session/init', (req, res) => {
-    // Before doing anything, check if a session already exists
-    // Get session cookie
-    const sessionCookie = req.signedCookies['session'];
-    let previousSession = appContext.sessionMgr.findSession(sessionCookie);
-
-    if(previousSession)
-        // Return previous session key
-        return sendResponseObject(res, 200, constructResponseObject(true, "", { id: previousSession.id }));
-
-    // Process new session
-    if((!req.body) || (typeof req.body.type !== 'number'))
-        // Return bad request - type must be specified
-        return sendResponseObject(res, 400, constructResponseObject(false, "Invalid session type specified."));
-
-    // Get session type
-    switch(req.body.type) {
-        default:
-            // Unknown type
-            res.status(400);
-            res.end();
-            break;
-        case MODUS_SESSION_TYPE.ADMINISTRATION:
-            // TODO Check for admin secret here
-            // This will ensure normal users cannot logon as administrator
-            throw new Error("Not implemented - check admin auth secret");
-        case MODUS_SESSION_TYPE.KITCHEN_VIEW:
-            // TODO Check for kitchen view auth secret
-            throw new Error("Not implemented - check kitchen auth secret");
-        case MODUS_SESSION_TYPE.USER:
-            // Normal users are welcome
-            break;
-    }
-
-    // Create the session and send it
-    const newSessionId = appContext.sessionMgr.createSession(req.body.type).id;
-    res.cookie('session', newSessionId, { signed: true });
-    return sendResponseObject(res, 200, constructResponseObject(true, "", {
-        id: newSessionId
-    }));
-});
-
+// [ Load routes ]
+loadAllRoutes(appContext, [
+    "routes/session"
+]);
 
 /**
  * Developer mode switch.
@@ -103,6 +61,9 @@ if(DEV_MODE) {
         res.sendFile(path.join(__dirname, config.frontend.path, "index.html"));
     });
 }
+
+// Custom error middleware
+app.use(require('./lib/middleware/error-handler'));
 
 /**
  * Gateway entry point.
