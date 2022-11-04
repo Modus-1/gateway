953e71ada4d42365e5c788707a887e54322cbcba
ralph <ralph@localhost>
2022-11-04 09:36:07 +0100

    basic setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Modus gateway service
+
+This is a gateway service.
+
+## Initializing
+
+Run `npm install` to install dependencies.
+
+## Running
+
+Run `node index.js` to start this service.
---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ * MODUS ASSUMPTION GATEWAY SERVER.
+ */
+
+const express = require('express');
+const app = express();
+const config = require('./data/config.json');
+const { logInfo } = require('./lib/logger');
+
+/**
+ * Gateway entry point.
+ */
+async function main() {
+    app.listen(config.http.port, ()=> {
+        logInfo(`Gateway server is active @ port ${config.http.port}`);
+    });
+}
+
+main();
